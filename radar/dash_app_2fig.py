5ced5a8d01f6ede369e88004ca95668b22d87f0c
dilaradupont <dilara.dupont@icloud.com>
2022-02-03 17:04:36 +0000

    Fixed no selection issue.

--- radar/dash_app_2fig.py
+++ radar/dash_app_2fig.py
@@ -78,56 +78,56 @@ app.layout = radar_page
 
 @app.callback(
     [Output("radar_chart", "figure"), Output("radar_chart1", "figure")],
-    [Input("country", "value")], [Input("year", "value")], [Input("country1", "value")], [Input("year1", "value")])
+    [Input("country", "value")], [Input("year", "value")],
+     [Input("country1", "value")], [Input("year1", "value")])
 def update_chart(country, year, country1, year1):
-    # if len(country) == 0:
-    #     country = ['Afghanistan']
-    # if len(year) == 0:
-    #     year = ['2020']
-
-    # if len(country1) == 0:
-    #     country1 = ['Afghanistan']
-    # if len(year1) == 0:
-    #     year1 = ['2019']
-
-    if country == '':
-        country = 'Afghanistan'
-    if year == '':
-        year = '2019'
-    if year1 == '':
-        year1 = '2019'
-        year1 = '2020'
+    if isinstance(country, list):
+        if len(country) == 0:
+            country = 'Afghanistan'
+    else:
+        if country == '':
+            country = 'Afghanistan'
+    if isinstance(country1, list):
+        if len(country1) == 0:
+            country1 = 'Afghanistan'
+    else:
+        if country1 == '':
+            country1 = 'Afghanistan'
+    if isinstance(year, int):
+        if year == '':
+            year = '2019'
+    else:
+        if len(year) == 0:
+            year = '2019'
+    if isinstance(year1, int):
+        if year1 == '':
+            year1 = '2020'
+    else:
+        if len(year1) == 0:
+            year1 = '2020'
 
     lis1 = [str(country), str(country1)]
     lis2 = [str(year), str(year1)]
-    print(country)
 
-    indicators_title = ['Time required score','Procedures required score','Cost (% of income per capita) score']
+    indicators_title = ['Time required score','Procedures required score',
+    'Cost (% of income per capita) score']
     country_list = df_radar['Country Name'].unique().tolist()
     lis_fig = []
 
     for i in range(len(lis1)):
-        # Get data
-        # year = ''.join(filter(str.isalnum, str(lis2[i])))
-        # country = ''.join(filter(str.isalnum, str(lis1[i])))
         year = lis2[i]
         country = lis1[i]
-
-        # Default graph settings
-        # if country == '':
-        #     country = 'Afghanistan'
-        # if year == '' and i == 0:
-        #     year = '2019'
-        # if year == '' and i == 1:
-        #     year = '2020'
 
         header = df_radar.columns.tolist()
         col = header.index(year)
         row = country_list.index(country)
-        afg_idx = df_radar.index[df_radar['Country Name'] == df_radar.iloc[0, 0]].tolist()
+        afg_idx = df_radar.index[df_radar['Country Name']
+         == df_radar.iloc[0, 0]].tolist()
 
-        radar_m = [df_radar.iloc[afg_idx[0]+row, col], df_radar.iloc[afg_idx[2]+row, col], df_radar.iloc[afg_idx[4]+row, col]]
-        radar_w = [df_radar.iloc[afg_idx[1]+row, col], df_radar.iloc[afg_idx[3]+row, col], df_radar.iloc[afg_idx[5]+row, col]]
+        radar_m = [df_radar.iloc[afg_idx[0]+row, col],
+         df_radar.iloc[afg_idx[2]+row, col], df_radar.iloc[afg_idx[4]+row, col]]
+        radar_w = [df_radar.iloc[afg_idx[1]+row, col],
+         df_radar.iloc[afg_idx[3]+row, col], df_radar.iloc[afg_idx[5]+row, col]]
 
         fig = go.Figure()
 
@@ -136,7 +136,7 @@ def update_chart(country, year, country1, year1):
             theta=indicators_title,
             line_color='blue',
             fill='toself',
-            name=f'Men {df_radar.iloc[0+row, 0]}'
+            name=f'Men'
         ))
 
         fig.add_trace(go.Scatterpolar(
@@ -144,11 +144,13 @@ def update_chart(country, year, country1, year1):
             theta=indicators_title,
             line_color='red',
             fill='toself',
-            name=f'Women {df_radar.iloc[0+row, 0]}'
+            name=f'Women'
         ))
 
         fig.update_layout(
-            title= f'Scores for setting up a business according to gender in {df_radar.iloc[0+row,0]} in {header[col]}',
+            title_text= f'Scores for setting up a business according to gender\
+             in <br>{df_radar.iloc[0+row,0]} in {header[col]}',
+            title_x=0.5,
             polar=dict(
                 radialaxis=dict(
                 visible=True,
