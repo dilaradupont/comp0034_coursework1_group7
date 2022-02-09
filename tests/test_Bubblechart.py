c27e80c0414a7b6931a9d50d62996be5f304d0d9
katevanelli <caterina.coralli.19@ucl.ac.uk>
2022-02-09 13:16:03 +0000

    testing using multiple urls

--- tests/test_Bubblechart.py
+++ tests/test_Bubblechart.py
@@ -7,4 +7,4 @@ def test_rere001_h1textequals(dash_duo, run_bubble_app):
     dash_duo.wait_for_element("H5", timeout=4)
     h1_text = dash_duo.find_element("H5").text
     dash_duo.driver.implicitly_wait(3)
-    assert h1_text.casefold() == "Select Region".casefold()
+    assert h1_text.casefold() == "Select Gender".casefold()
