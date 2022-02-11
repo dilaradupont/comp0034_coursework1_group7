ba3b22e65027a3506afc0abb0dd69deb5bca4138
Andrearipa <91899514+Andrearipa@users.noreply.github.com>
2022-02-11 20:09:59 +0000

    github check

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ def pytest_setup_options():
     # Uncomment the following if testing on GitHub actions, the browser needs to run in headless mode
     options.add_argument('--disable-gpu')
     options.add_argument('--headless') # this is commented out to automatically open the test in a new chrome window
-    options.add_argument('--start-maximized')
+    # options.add_argument('--start-maximized')
     return options
 
 
